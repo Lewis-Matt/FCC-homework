e3b9b81cdd8192356b25c2d0d47a6cc20fefca6b
Matt <29003130+misterlewis07@users.noreply.github.com>
2021-10-15 21:34:45 +0200

    Update basicAlgorithmScripts.js

--- basicAlgorithmScripts.js
+++ basicAlgorithmScripts.js
@@ -1,8 +1,9 @@
-/**************************
+/***************************************************************************************************************************
 Reverse the provided string.
 You may need to turn the string into an array before you can reverse it.
 Your result must be a string.
 */
+
 // The join() method creates and returns a new string by concatenating all of the elements in an array
 // syntax: join(separator)
 function reverseString(str) {
@@ -10,3 +11,34 @@ function reverseString(str) {
   return backwards.reverse().join("");
 }
 reverseString("hello");
+
+/***************************************************************************************************************************
+Return the factorial of the provided integer.
+*/
+function factorialize(num) {
+  for (var product = 1; num > 0; num--){
+    product *= num;
+  }
+  return product;
+}
+
+factorialize(5);
+// Using recursion
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
+
+factorialize(5);
+/* 
+---Call Stack---
+factorialize(5)
+  5factorialize(4)
+    4factorialize(3)
+      3factorialize(2)
+        2factorialize(1)
+          1 --- Base Case
+*/
+/***************************************************************************************************************************
