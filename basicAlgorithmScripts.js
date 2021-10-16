d31dedcf82d0b7d68bf67af440584d77130572d0
Matt <29003130+misterlewis07@users.noreply.github.com>
2021-10-16 11:30:35 +0200

    Update basicAlgorithmScripts.js

--- basicAlgorithmScripts.js
+++ basicAlgorithmScripts.js
@@ -42,3 +42,22 @@ factorialize(5)
           1 --- Base Case
 */
 /***************************************************************************************************************************
+The split() method divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array.  
+The division is done by searching for a pattern; where the pattern is provided as the first parameter in the method's call. */
+
+function findLongestWordLength(str) {
+  // Splits str into an array of substrings, using a ' ' (blank space) as the breaking/splitting point
+  let splitWords = str.split(' ');
+  // For debugging
+  console.log(splitWords);
+  let longestWord = 0;
+  for (var i = 0; i < splitWords.length; i++) {
+    if (splitWords[i].length > longestWord) {
+      longestWord = splitWords[i].length;
+    }
+  }
+  return longestWord;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+/***************************************************************************************************************************
