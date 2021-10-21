634039993ed1dee5b396e9b94c1ca9bdcd343af8
Matt <29003130+misterlewis07@users.noreply.github.com>
2021-10-21 16:28:14 +0200

    Update basicAlgorithmScripts.js

--- basicAlgorithmScripts.js
+++ basicAlgorithmScripts.js
@@ -339,3 +339,26 @@ function chunkArrayInGroups(arr, size) {
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
 /***************************************************************************************************************************
+Add all of the own properties of beagle to the array ownProps. Add all of the prototype properties of Dog to the array prototypeProps. */
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+for (let property in beagle) {
+  if(beagle.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
+
+/***************************************************************************************************************************
